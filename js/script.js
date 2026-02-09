fa702c38d8eac6d459f22f597571120ff7a0cfa6
jonnyjavi <jonathanjavier.calvasoto@gmail.com>
2026-02-09 20:39:29 +0100

    Conexión con Api

--- js/script.js
+++ js/script.js
@@ -2,21 +2,33 @@ const boton = document.getElementById("botonCambiar");
 const mensaje = document.getElementById("mensaje");
 const input = document.getElementById("userName");
 
-boton.addEventListener("click", () => {
+boton.addEventListener("click", async () => {
 const usuario = input.value.trim();
 
   // Mostrar imagen de transición
   transicionImg.style.display = "block";
 
   // Oculta la imagen después de 2.5 segundos y ejecuta el resto
-  setTimeout(() => {
+  setTimeout(async () => {
     transicionImg.style.display = "none";
 
     // Cambia el mensaje y los estilos:
     if (usuario && !mensaje.classList.contains("mensaje-rojo")) {
-      mensaje.textContent = "Hola " + usuario + ", no deberías haber accedido aquí";
-      mensaje.classList.add("mensaje-rojo"); // Aplica el gradiente rojo a las letras
-      input.style.display = "none";
+      try {
+        const response = await fetch(`http://localhost:8080/api/advertencias?nombre=${encodeURIComponent(usuario)}`);
+        const data = await response.json();
+        
+        if (data.estado === "denied") {
+          mensaje.textContent = data.mensaje;
+          mensaje.classList.add("mensaje-rojo"); // Aplica el gradiente rojo a las letras
+          input.style.display = "none";
+        }
+      } catch (error) {
+        console.error("Error al obtener el mensaje de la API:", error);
+        mensaje.textContent = "Error al obtener el saludo";
+        mensaje.classList.add("mensaje-rojo");
+        input.style.display = "none";
+      }
     } else {
       mensaje.textContent = "Bienvenido a mi página web";
       mensaje.classList.remove("mensaje-rojo"); // Vuelve al color original
